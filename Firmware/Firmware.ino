6c1ca53bf80cd09557cabc15e75209d8d7841b63
Renata Faraco Cunha <renatafaraco@gmail.com>
2017-09-23 18:20:33 -0300

    adicionando html

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,43 +1,59 @@
-#include <IRremote.h>
+#include <IRremoteESP8266.h>
 #include <MideaIR.h>
+
 #define IR_EMITER 3
+#define LED 4
 #define FIVE_SECONDS    5000
 
 // IRsend Object and Remote Control object
-IRsend irsend;
+IRsend irsend(IR_EMITER);
 MideaIR remote_control(&irsend);
 
+void piscaled() {
+  for(int i = 0; i < 5; i++) {
+    digitalWrite(LED,HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
+}
+
 void setup(){
+  Serial.begin(9600);
+  
   // Define IR PIN as Output
   pinMode(IR_EMITER, OUTPUT);
-
+  pinMode(LED, OUTPUT);
+  
   // Try to turn ON the Air Conditioner
   Serial.println(F("Trying to Turn ON the Air Conditioner..."));
   remote_control.turnON();
-   delay(FIVE_SECONDS);
+  piscaled();
+
+  // Change temperature to 22
+  Serial.println(F("Set temperature to 22"));
+  remote_control.setTemperature(22);
+  remote_control.emit();
+  piscaled();
+
+  // Change mode
+  Serial.println(F("Set mode to auto"));
+  remote_control.setMode(mode_auto);
+  remote_control.emit();
+  piscaled();
+  
+  // Change speed fan
+  Serial.println(F("Set fan speed to 1"));
+  remote_control.setSpeedFan(fan_speed_1);
+  remote_control.emit();
+  piscaled();
 
   // Try to turn OFF
   Serial.println(F("Trying to Turn OFF the Air Conditioner..."));
   remote_control.turnOFF();
-  delay(FIVE_SECONDS);
+  piscaled();
   
-  // Change mode
-  remote_control.setState(true);
 
-  // Change mode
-  remote_control.setMode(mode_auto);
-  remote_control.emit();
-  delay(FIVE_SECONDS);
-  
-  // Change speed fan
-  remote_control.setSpeedFan(fan_speed_1);
-  remote_control.emit();
-  delay(FIVE_SECONDS);
-  
-  // Change temperature to 22
-  remote_control.setTemperature(22);
-  remote_control.emit();
-  delay(FIVE_SECONDS);
   
 }
 
